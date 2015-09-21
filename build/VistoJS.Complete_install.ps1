9bead0f8776feb03e46d6d1954a124da51c0cfb5
Rico Suter <mail@rsuter.com>
2015-09-21 18:59:37 +0200

    Updated build script

--- build/VistoJS.Complete_install.ps1
+++ build/VistoJS.Complete_install.ps1
@@ -4,6 +4,3 @@ $folder = $project.ProjectItems.Item("Scripts").ProjectItems.AddFolder("app")
 $folder.ProjectItems.AddFolder("views")
 $folder.ProjectItems.AddFolder("viewModels")
 $folder.ProjectItems.AddFolder("languages")
-
-# $item = $project.ProjectItems.Item("Scripts").ProjectItems.Item("app.ts")
-# $item.Properties.Item("BuildAction").Value = [int]4
